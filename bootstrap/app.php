89a8107953e9bf135910c124ef9ba5eda538e6b1
Muammar Azmi Muhammad <azmimuhammad1207@gmail.com>
2026-06-20 12:59:52 +0700

    fix: trust railway proxies to resolve mixed content

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        
+        // Memaksa Laravel percaya pada Proxy Railway agar HTTPS/CSS tidak diblokir
+        $middleware->trustProxies(at: '*');
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
